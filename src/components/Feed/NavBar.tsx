a17b1444f3c6896d90433d4f5239248570785056
karipov <karipov@protonmail.com>
2022-07-21 00:46:05 +0200

    hamburger button init

--- src/components/Feed/NavBar.tsx
+++ src/components/Feed/NavBar.tsx
@@ -1,16 +1,37 @@
 import Link from "next/link";
+import { useState } from "react";
+
+// const burgerSvg;
 
 export default function NavBar() {
+  const [menu, setMenu] = useState(false);
+
   return (
-    <div className="w-full flex space-x-5 justify-end uppercase font-mono">
+    <div
+      className={`w-full flex ${
+        menu ? "flex-col sm:flex-row" : "flex-row"
+      } space-x-5 justify-end uppercase font-mono`}
+    >
       <p className="grow">Komron Aripov</p>
-      <Link href="/">
-        <a>/home</a>
-      </Link>
-      <Link href="/courses">
-        <a>/courses</a>
-      </Link>
-      <a href="">/resume</a>
+
+      {/* hamburger button */}
+      <button onClick={() => setMenu(!menu)} className="sm:hidden">
+        {menu ? "CLOSE" : "MENU"}
+      </button>
+
+      <div className={menu ? "" : "hidden sm:flex"}>
+        <Link href="/">
+          <a>/home</a>
+        </Link>
+      </div>
+      <div className={menu ? "" : "hidden sm:flex"}>
+        <Link href="/courses">
+          <a>/courses</a>
+        </Link>
+      </div>
+      <div className="hidden sm:flex">
+        <a href="">/resume</a>
+      </div>
     </div>
   );
 }
